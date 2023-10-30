57e25f3e4e4d0ec74be3e7904b8ed95a88915f08
ジェフリ　ジュニオル <34094468+jjmmfiis@users.noreply.github.com>
2023-10-30 18:38:05 -0500

    Update password.py

--- password.py
+++ password.py
@@ -6,7 +6,7 @@ Created on Wed Oct 18 10:29:28 2023
 """
 # password.py
 
-consumer_key="JAIWdrCmw8VxVbz40OJQd2X1J"
-consumer_secret="PLw0LYIpx1VJCI1fi10ZLhvvDxsaVhY5u5PDcSemD32WYgTfI3"
-access_token="2176522045-XqCdjpgJLiV4jXvJAGPerVf6M5gVPja0KLEkRJU"
-access_token_secret="9WztPGn8QicCX0lFTOrYLCXch5Hnf1MlORUnAl7iiLxNV"
+consumer_key="JAIWdrCmw8VxVbz40...."
+consumer_secret="PLw0LYIpx1VJCI1fi10ZLhvvDxsaV....."
+access_token="2176522045-XqCdjpgJLiV4jXvJAGPerVf6M....."
+access_token_secret="9WztPGn8QicCX0lFTOrYLCXch5Hnf1Ml....."
